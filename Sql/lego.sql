19d61ba54d58f2edea5e24831724f7b78e7a9093
Aviral Kumar <aviralkumar862@gmail.com>
2026-08-25 09:01:35 +0530

    Add files via upload

--- Sql/lego.sql
+++ Sql/lego.sql
@@ -1,1 +1,18 @@
-
+Create database lego;
+ CREATE TABLE lego__sets (
+    set_id VARCHAR(50) PRIMARY KEY,
+    name_ TEXT,
+    year_ INTEGER,
+    theme TEXT,
+    subtheme TEXT,
+    themeGroup TEXT,
+    category TEXT,
+    pieces INTEGER,
+    minifigs INTEGER,
+    agerange_min INTEGER,
+    US_retailPrice NUMERIC(10,2),
+    bricksetURL TEXT,
+    thumbnailURL TEXT,
+    imageURL TEXT
+);
+select * from lego__sets;
